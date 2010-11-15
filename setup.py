56282ed2c9a993a95ff00fec2c15d96ca628a9d5
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-11-15 09:05:22 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='Pykka',
+    version='0.1',
+    author='Stein Magnus Jodal',
+    author_email='stein.magnus@jodal.no',
+    packages=['pykka'],
+    url='http://github.com/jodal/pykka',
+    license='Apache License, Version 2.0',
+    description='Pykka makes actors look like regular objects',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
