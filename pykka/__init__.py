bf54918b39e43f4e16bba174c1b57dafc9b62314
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-12-07 00:03:00 +0100

    Document registry methods

--- pykka/__init__.py
+++ pykka/__init__.py
@@ -24,21 +24,30 @@ def unpickle_connection(pickled_connection):
 
 
 class ActorRegistry(object):
+    """
+    Registry which provides easy access to all running actors.
+
+    Contains global state, but should be thread-safe.
+    """
+
     _actors = []
     _actors_lock = threading.RLock()
 
     @classmethod
     def get_all(cls):
+        """Get all running actors"""
         with cls._actors_lock:
             return cls._actors[:]
 
     @classmethod
     def get_by_class(cls, actor_class):
+        """Get all running actors of the given class"""
         with cls._actors_lock:
             return filter(lambda a: a._actor_class == actor_class, cls._actors)
 
     @classmethod
     def get_by_class_name(cls, actor_class_name):
+        """Get all running actors of the given class name"""
         with cls._actors_lock:
             return filter(
                 lambda a: a._actor_class.__name__ == actor_class_name,
@@ -46,11 +55,21 @@ class ActorRegistry(object):
 
     @classmethod
     def register(cls, actor):
+        """
+        Register an actor in the registry.
+
+        This is done automatically when an actor is started.
+        """
         with cls._actors_lock:
             cls._actors.append(actor)
 
     @classmethod
     def unregister(cls, actor):
+        """
+        Remove an actor from the registry.
+
+        This is done automatically when an actor is stopped.
+        """
         with cls._actors_lock:
             cls._actors.remove(actor)
 
