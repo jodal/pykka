fe0d8537c1693a1f987bbfdfb4f2210f96b246b4
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-12-07 00:50:07 +0100

    Use new features in old tests

--- tests/registry_test.py
+++ tests/registry_test.py
@@ -1,10 +1,10 @@
 import unittest
 
-from pykka import Actor, ActorRegistry
+import pykka
 
 class ActorRegistrationTest(unittest.TestCase):
     def setUp(self):
-        class AnActor(Actor):
+        class AnActor(pykka.Actor):
             pass
         self.actor = AnActor().start()
 
@@ -12,23 +12,23 @@ class ActorRegistrationTest(unittest.TestCase):
         self.actor.stop()
 
     def test_actor_is_registered_when_started(self):
-        self.assert_(self.actor in ActorRegistry.get_all())
+        self.assert_(self.actor in pykka.ActorRegistry.get_all())
 
     def test_actor_is_unregistered_when_stopped(self):
-        self.assert_(self.actor in ActorRegistry.get_all())
+        self.assert_(self.actor in pykka.ActorRegistry.get_all())
         self.actor.stop().wait()
-        self.assert_(self.actor not in ActorRegistry.get_all())
+        self.assert_(self.actor not in pykka.ActorRegistry.get_all())
 
     def test_actor_may_be_registered_manually(self):
-        ActorRegistry.unregister(self.actor)
-        self.assert_(self.actor not in ActorRegistry.get_all())
-        ActorRegistry.register(self.actor)
-        self.assert_(self.actor in ActorRegistry.get_all())
+        pykka.ActorRegistry.unregister(self.actor)
+        self.assert_(self.actor not in pykka.ActorRegistry.get_all())
+        pykka.ActorRegistry.register(self.actor)
+        self.assert_(self.actor in pykka.ActorRegistry.get_all())
 
 
 class ActorLookupTest(unittest.TestCase):
-    class AnActor(Actor): pass
-    class BeeActor(Actor): pass
+    class AnActor(pykka.Actor): pass
+    class BeeActor(pykka.Actor): pass
 
     def setUp(self):
         self.a_actors = [self.AnActor().start() for i in range(3)]
@@ -38,14 +38,14 @@ class ActorLookupTest(unittest.TestCase):
         map(lambda a: a.stop(), self.a_actors + self.b_actors)
 
     def test_actors_may_be_looked_up_class(self):
-        result = ActorRegistry.get_by_class(self.AnActor)
+        result = pykka.ActorRegistry.get_by_class(self.AnActor)
         for a_actor in self.a_actors:
             self.assert_(a_actor in result)
         for b_actor in self.b_actors:
             self.assert_(b_actor not in result)
 
     def test_actors_may_be_looked_up_class_name(self):
-        result = ActorRegistry.get_by_class_name('AnActor')
+        result = pykka.ActorRegistry.get_by_class_name('AnActor')
         for a_actor in self.a_actors:
             self.assert_(a_actor in result)
         for b_actor in self.b_actors:
@@ -54,11 +54,10 @@ class ActorLookupTest(unittest.TestCase):
 
 class ActorStoppingTest(unittest.TestCase):
     def setUp(self):
-        class AnActor(Actor): pass
+        class AnActor(pykka.Actor): pass
         self.actors = [AnActor().start() for i in range(3)]
 
     def test_all_actors_can_be_stopped_through_registry(self):
-        self.assertEquals(3, len(ActorRegistry.get_all()))
-        futures = ActorRegistry.stop_all()
-        map(lambda f: f.wait(), futures)
-        self.assertEquals(0, len(ActorRegistry.get_all()))
+        self.assertEquals(3, len(pykka.ActorRegistry.get_all()))
+        pykka.wait_all(pykka.ActorRegistry.stop_all())
+        self.assertEquals(0, len(pykka.ActorRegistry.get_all()))
