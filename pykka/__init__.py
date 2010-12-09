7e1a83029d5c517abcb36df37807ccf0376178cf
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-12-09 22:42:05 +0100

    Release v0.5

--- pykka/__init__.py
+++ pykka/__init__.py
@@ -8,7 +8,7 @@ __all__ = ['Actor', 'ActorProxy', 'ActorRegistry', 'CallableProxy', 'Future',
     'get_all', 'wait_all']
 
 
-VERSION = (0, 4)
+VERSION = (0, 5)
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
