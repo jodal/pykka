d76c7d0367ff95194bcd6a26125c2b2bf03d9819
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-02-19 21:16:25 +0100

    Update docs copyright

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Pykka'
-copyright = u'2010, Stein Magnus Jodal'
+copyright = u'2010-2011, Stein Magnus Jodal'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
