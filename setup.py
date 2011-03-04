76dddab86552a1e65cdc8ed26bb0674c83d5697b
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-03-05 00:25:53 +0100

    Update description and categories for PyPI

--- setup.py
+++ setup.py
@@ -10,8 +10,7 @@ setup(
     packages=['pykka'],
     url='http://jodal.github.com/pykka/',
     license='Apache License, Version 2.0',
-    description='Pykka is a concurrency abstraction which let you use ' +
-        'concurrent actors like regular objects',
+    description='Pykka is easy to use concurrency using the actor model',
     long_description=open('README.rst').read(),
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -19,6 +18,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries',
     ],
 )
