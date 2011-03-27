fe27808f5e0ec3dfccb9769cef268c7bb332054e
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-03-27 03:10:15 +0200

    Ignore 'pykka' prefix in docs' modules index

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
+modindex_common_prefix = ['pykka.']
 
 
 # -- Options for HTML output ---------------------------------------------------
