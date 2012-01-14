04e5977948f29f2f0fcf4fc971bd3fd35e6b317d
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-01-14 23:53:26 +0100

    Improve the RTD dependency mock

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,30 @@
 
 import sys, os
 
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(self, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = [
+    'gevent',
+    'gevent.event',
+    'gevent.queue',
+]
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -222,23 +246,3 @@ man_pages = [
 autodoc_member_order = 'bysource'
 
 extlinks = {'issue': ('http://github.com/jodal/pykka/issues/%s', 'GH-')}
-
-
-# Let autodoc generate docs for modules with external dependencies without #
-# installing those dependencies. Trick from
-# http://read-the-docs.readthedocs.org/en/latest/faq.html
-
-class Mock(object):
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(self, name):
-        return Mock() if name not in ('__file__', '__path__') else '/dev/null'
-
-MOCK_MODULES = ['gevent']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = Mock()
