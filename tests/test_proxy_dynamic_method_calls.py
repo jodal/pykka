acbf90ee83da4db2f1bb62796ed47488bd2693e5
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-01-20 23:43:33 +0100

    tests: Port method call tests to pytest

---
+++ tests/test_proxy_dynamic_method_calls.py
@@ -0,0 +1,42 @@
+import pytest
+
+
+@pytest.fixture(scope='module')
+def actor_class(runtime):
+    class ActorA(runtime.actor_class):
+        def add_method(self, name):
+            setattr(self, name, lambda: 'returned by ' + name)
+
+        def use_foo_through_self_proxy(self):
+            return self.actor_ref.proxy().foo()
+
+    return ActorA
+
+
+@pytest.fixture
+def proxy(actor_class):
+    proxy = actor_class.start().proxy()
+    yield proxy
+    proxy.stop()
+
+
+def test_can_call_method_that_was_added_at_runtime(proxy):
+    # We need to .get() after .add_method() to be sure that the method has
+    # been added before we try to use it through the proxy.
+    proxy.add_method('foo').get()
+
+    assert proxy.foo().get() == 'returned by foo'
+
+
+def test_can_proxy_itself_and_use_attrs_added_at_runtime(proxy):
+    # We don't need to .get() after .add_method() here, because the actor
+    # will process the .add_method() call before processing the
+    # .use_foo_through_self_proxy() call, which again will use the new
+    # method, .foo().
+    proxy.add_method('foo')
+
+    outer_future = proxy.use_foo_through_self_proxy()
+    inner_future = outer_future.get(timeout=1)
+    result = inner_future.get(timeout=1)
+
+    assert result == 'returned by foo'
---
+++ tests/test_proxy_static_method_calls.py
@@ -0,0 +1,60 @@
+import pytest
+
+
+@pytest.fixture(scope='module')
+def actor_class(runtime):
+    class ActorA(runtime.actor_class):
+        cat = 'dog'
+
+        def functional_hello(self, s):
+            return 'Hello, %s!' % s
+
+        def set_cat(self, s):
+            self.cat = s
+
+        def raise_keyboard_interrupt(self):
+            raise KeyboardInterrupt
+
+        def talk_with_self(self):
+            return self.actor_ref.proxy().functional_hello('from the future')
+
+    return ActorA
+
+
+@pytest.fixture
+def proxy(actor_class):
+    proxy = actor_class.start().proxy()
+    yield proxy
+    proxy.stop()
+
+
+def test_functional_method_call_returns_correct_value(proxy):
+    assert proxy.functional_hello('world').get() == 'Hello, world!'
+    assert proxy.functional_hello('moon').get() == 'Hello, moon!'
+
+
+def test_side_effect_of_method_is_observable(proxy):
+    assert proxy.cat.get() == 'dog'
+
+    proxy.set_cat('eagle')
+
+    assert proxy.cat.get() == 'eagle'
+
+
+def test_calling_unknown_method_raises_attribute_error(proxy):
+    with pytest.raises(AttributeError) as exc_info:
+        proxy.unknown_method()
+
+    result = str(exc_info.value)
+
+    assert result.startswith('<ActorProxy for ActorA')
+    assert result.endswith('has no attribute "unknown_method"')
+
+
+def test_can_proxy_itself_for_offloading_work_to_the_future(proxy):
+    outer_future = proxy.talk_with_self()
+    inner_future = outer_future.get(timeout=1)
+
+    result = inner_future.get(timeout=1)
+
+    assert result == 'Hello, from the future!'
