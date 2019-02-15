329af558b1a186d182e389108d93c5d70088e8c9
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-02-15 14:04:37 +0100

    proxy: Improve tests of mocks accessed through proxy

--- tests/test_proxy_attribute_access.py
+++ tests/test_proxy_attribute_access.py
@@ -1,12 +1,14 @@
 import pytest
 
+from pykka import compat
+
 
 class NestedObject(object):
     pykka_traversable = True
     inner = 'nested.inner'
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture
 def actor_class(runtime):
     class ActorA(runtime.actor_class):
         an_attr = 'an_attr'
@@ -26,6 +28,9 @@ def actor_class(runtime):
         @a_rw_property.setter
         def a_rw_property(self, value):
             self._a_rw_property = value
+
+        def a_method(self):
+            raise Exception('This method should be mocked')
 
     return ActorA
 
@@ -79,30 +84,68 @@ def test_actor_read_only_property_cannot_be_set(proxy):
         proxy.a_ro_property = 'a_ro_property_2'
 
 
-def test_actor_with_mock_attribute_works(runtime, mocker):
-    class Actor(runtime.actor_class):
-        attr = mocker.Mock()
+def test_actor_with_noncallable_mock_property_works(
+    actor_class, stop_all, mocker
+):
+    mock = mocker.NonCallableMock()
+    mock.__get__ = mocker.Mock(return_value='mocked property value')
+    assert not isinstance(mock, compat.Callable)
 
-    actor_ref = Actor.start()
-    try:
-        actor_ref.proxy()
-    finally:
-        actor_ref.stop()
+    actor_class.a_rw_property = mock
+    proxy = actor_class.start().proxy()
+
+    # When using NonCallableMock to fake the property, the value still behaves
+    # as a property when access through the proxy.
+    assert proxy.a_rw_property.get() == 'mocked property value'
+
+    assert mock.__get__.call_count == 1
 
 
-def test_actor_with_mocked_method_works(runtime, mocker):
-    class Actor(runtime.actor_class):
-        def do_it(self):
-            pass
+def test_actor_with_callable_mock_property_does_not_work(
+    actor_class, stop_all, mocker
+):
+    mock = mocker.Mock()
+    mock.__get__ = mocker.Mock(return_value='mocked property value')
+    assert isinstance(mock, compat.Callable)
 
-    with mocker.patch.object(Actor, 'do_it') as mock:
-        actor_ref = Actor.start()
-        try:
-            actor_ref.proxy()
-        finally:
-            actor_ref.stop()
+    actor_class.a_rw_property = mock
+    proxy = actor_class.start().proxy()
 
-        assert mock.call_count == 0
+    # XXX Because Mock and MagicMock are callable by default, they cause the
+    # property to be wrapped in a `_CallableProxy`. Thus, the property no
+    # longer behaves as a property when mocked and accessed through a proxy.
+    with pytest.raises(AttributeError) as exc_info:
+        assert proxy.a_rw_property.get()
+
+    assert "'_CallableProxy' object has no attribute 'get'" in str(
+        exc_info.value
+    )
+
+
+def test_actor_with_mocked_method_works(actor_class, stop_all, mocker):
+    mock = mocker.MagicMock(return_value='mocked method return')
+    with mocker.patch.object(actor_class, 'a_method', new=mock):
+        proxy = actor_class.start().proxy()
+
+        assert proxy.a_method().get() == 'mocked method return'
+
+    assert mock.call_count == 1
+
+
+def test_actor_with_mock_created_by_patch_does_not_work(
+    actor_class, stop_all, mocker
+):
+    with mocker.patch.object(
+        actor_class, 'a_method', return_value='foo'
+    ) as mock:
+        proxy = actor_class.start().proxy()
+
+        # The method is properly mocked, as seen from the return value
+        assert proxy.a_method().get() == 'foo'
+
+    # XXX But, with a mock created by the `mock.patch.object()` context
+    # manager, we cannot inspect the calls. Patches welcome!
+    assert mock.call_count == 0
 
 
 def test_actor_property_is_not_accessed_when_creating_proxy(runtime):
