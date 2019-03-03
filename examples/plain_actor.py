bcf4c5e632ae3ee678ac10e93887b14c63d4eb4a
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-03-03 09:08:14 +0100

    examples: Use custom message instead of dict

--- examples/plain_actor.py
+++ examples/plain_actor.py
@@ -3,13 +3,16 @@
 import pykka
 
 
+GetMessages = object()
+
+
 class PlainActor(pykka.ThreadingActor):
     def __init__(self):
         super().__init__()
         self.stored_messages = []
 
     def on_receive(self, message):
-        if message.get('command') == 'get_messages':
+        if message is GetMessages:
             return self.stored_messages
         else:
             self.stored_messages.append(message)
@@ -19,5 +22,5 @@ if __name__ == '__main__':
     actor = PlainActor.start()
     actor.tell({'no': 'Norway', 'se': 'Sweden'})
     actor.tell({'a': 3, 'b': 4, 'c': 5})
-    print(actor.ask({'command': 'get_messages'}))
+    print(actor.ask(GetMessages))
     actor.stop()
