211e222253ddf04e5853eecc3c22f6d0d4b2bfb9
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-03-28 22:44:02 +0100

    Remove unused code
    Introduced by accident in commit
    e6c51e9da383c0c1aa2356431888412a2a626544.

--- pykka/_envelope.py
+++ pykka/_envelope.py
@@ -1,7 +1,3 @@
-import warnings
-from collections import namedtuple
-
-
 class Envelope(object):
     """
     Envelope to add metadata to a message.
@@ -25,50 +21,3 @@ class Envelope(object):
         return 'Envelope(message={!r}, reply_to={!r})'.format(
             self.message, self.reply_to
         )
-
-
-# Internal actor messages
-ActorStop = namedtuple('ActorStop', [])
-
-# Internal proxy messages
-ProxyCall = namedtuple('ProxyCall', ['attr_path', 'args', 'kwargs'])
-ProxyGetAttr = namedtuple('ProxyGetAttr', ['attr_path'])
-ProxySetAttr = namedtuple('ProxySetAttr', ['attr_path', 'value'])
-
-
-def upgrade_internal_message(message):
-    """Filter that upgrades dict-based internal messages to the new format.
-
-    This is needed for a transitional period because Mopidy < 3 uses
-    the old internal message format directly, and maybe others.
-    """
-
-    if not isinstance(message, dict):
-        return message
-    if not message.get('command', '').startswith('pykka_'):
-        return message
-
-    warnings.warn(
-        'Pykka received a dict-based internal message. '
-        'This is deprecated and will be unsupported in the future. '
-        'Message: {!r}'.format(message),
-        DeprecationWarning,
-    )
-
-    command = message.get('command')
-    if command == 'pykka_stop':
-        return ActorStop()
-    elif command == 'pykka_call':
-        return ProxyCall(
-            attr_path=message['attr_path'],
-            args=message['args'],
-            kwargs=message['kwargs'],
-        )
-    elif command == 'pykka_getattr':
-        return ProxyGetAttr(attr_path=message['attr_path'])
-    elif command == 'pykka_setattr':
-        return ProxySetAttr(
-            attr_path=message['attr_path'], value=message['value']
-        )
-    else:
-        raise ValueError('Unknown internal message: {!r}'.format(message))
