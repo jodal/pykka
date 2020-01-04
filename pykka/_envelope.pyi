0b799eebe69305b93b2e472f805bcccf37dd8a4e
Stein Magnus Jodal <stein.magnus@jodal.no>
2020-01-04 01:22:19 +0100

    typing: Add hints to envelope module

---
+++ pykka/_envelope.pyi
@@ -0,0 +1,11 @@
+from typing import Any, Optional
+
+from pykka import Future
+
+class Envelope:
+    message: Any
+    reply_to: Optional[Future]
+    def __init__(
+        self, message: Any, reply_to: Optional[Future] = ...
+    ) -> None: ...
+    def __repr__(self) -> str: ...
