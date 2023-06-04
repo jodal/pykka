11c36dff11c3efb3a8ecda5fc42252f669c9e851
Stein Magnus Jodal <stein.magnus@jodal.no>
2023-06-04 15:13:40 +0200

    Type pykka._envelope

--- src/pykka/_envelope.pyi
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-from pykka import Future
-
-class Envelope:
-    message: Any
-    reply_to: Future | None
-    def __init__(self, message: Any, reply_to: Future | None = ...) -> None: ...
