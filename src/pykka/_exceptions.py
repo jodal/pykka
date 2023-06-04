bcd3c3078bb03585d99b6e16ceaf90257a81de53
Stein Magnus Jodal <stein.magnus@jodal.no>
2023-06-04 13:18:16 +0200

    Fix N lints

--- src/pykka/_exceptions.py
+++ src/pykka/_exceptions.py
@@ -5,5 +5,5 @@ class ActorDeadError(Exception):
     """Exception raised when trying to use a dead or unavailable actor."""
 
 
-class Timeout(Exception):
+class Timeout(Exception):  # noqa: N818
     """Exception raised at future timeout."""
